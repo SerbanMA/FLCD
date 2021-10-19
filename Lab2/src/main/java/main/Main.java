9ea20678204a498165b214f07c7066c6e3bc24b0
Serban Maerean <maerean_serban_a@yahoo.com>
2021-10-19 14:45:37 +0300

    Lab3

---
+++ Lab2/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main.java.main;
+
+import main.java.Scanner;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner();
+
+        scanner.scan(new File(""));
+
+    }
+}
