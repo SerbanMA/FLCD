432c41c0204c3ad8349d2d24f31ad295d1c5a995
Serban Maerean <maerean_serban_a@yahoo.com>
2021-10-22 10:52:09 +0300

    Lab2

--- Lab2/src/main/java/helper/ParseHelper.java
+++ Lab2/src/main/java/helper/ParseHelper.java
@@ -2,19 +2,41 @@ package main.java.helper;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ParseHelper {
+
+    List<String> specials = List.of("/", ".", "*", "+", "?", "|", "(", ")", "[", "]", "{", "}", "\\");
 
     private final StringBuilder separatorsRegex = new StringBuilder();
     private final StringBuilder operatorsRegex = new StringBuilder();
 
-    public String[] splitWithOperators (String value) {
-        return value.split("(?<=(" + operatorsRegex + "))|(?=(" + operatorsRegex + "))");
+    public ParseHelper() {
+        readSeparators();
+        readOperators();
     }
 
-    public String[] splitWithSeparators (String value) {
-        return value.split("(?<=(" + separatorsRegex + "))|(?=(" + separatorsRegex + "))");
+    public ArrayList<String> parse(String value) {
+        return splitWithSeparators(String.join(" ", splitWithOperators(value)));
+    }
+
+
+    public ArrayList<String> splitWithOperators (String value) {
+        return Pattern.compile("(?<=(" + operatorsRegex + "))|(?=(" + operatorsRegex + "))")
+                .splitAsStream(value)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public ArrayList<String> splitWithSeparators (String value) {
+        List<String> stringList = Pattern.compile(separatorsRegex.toString())
+                .splitAsStream(value)
+                .collect(Collectors.toList());
+
+        return stringList.stream().
+                filter(element -> !element.isEmpty())
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
     private void readSeparators () {
@@ -23,11 +45,9 @@ public class ParseHelper {
             File myObj = new File("src/resources/files/input/separators.ini");
             Scanner myReader = new Scanner(myObj);
 
-            while (myReader.hasNextLine()) {
-                String operator = myReader.nextLine();
+            while (myReader.hasNextLine())
+                separatorsRegex.append("|").append(myReader.nextLine());
 
-                separatorsRegex.append("|").append(operator);
-            }
             separatorsRegex.deleteCharAt(0);
 
         } catch (FileNotFoundException exception) {
@@ -41,15 +61,20 @@ public class ParseHelper {
             File myObj = new File("src/resources/files/input/operators.ini");
             Scanner myReader = new Scanner(myObj);
 
-            while (myReader.hasNextLine()) {
-                String operator = myReader.nextLine();
+            while (myReader.hasNextLine())
+                operatorsRegex.append("|").append(myReader.nextLine());
 
-                operatorsRegex.append("|").append(operator);
-            }
             operatorsRegex.deleteCharAt(0);
 
         } catch (FileNotFoundException exception) {
             System.out.println(exception.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        ParseHelper parseHelper = new ParseHelper();
+
+
+        System.out.println(parseHelper.parse("anaandmaria<= [[[[ mere   ;eu~~tu[][+1 \n maria <-2add7 "));
     }
 }
--- Lab2/src/main/java/helper/RegexHelper.java
+++ Lab2/src/main/java/helper/RegexHelper.java
@@ -1,10 +1,5 @@
 package main.java.helper;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class RegexHelper {
 
     public RegexHelper() {}
@@ -23,29 +18,5 @@ public class RegexHelper {
     
     public boolean isString (String value) {
         return value.matches("^\"[a-zA-Z0-9 ]*\"$");
-    }
-
-    public static void main (String[] args) {
-
-        RegexHelper regexHelper = new RegexHelper();
-
-        System.out.println(regexHelper.isBoolean("1"));
-        System.out.println(regexHelper.isBoolean("0"));
-        System.out.println(regexHelper.isBoolean("01"));
-        System.out.println(regexHelper.isBoolean("true"));
-        System.out.println(regexHelper.isBoolean("false"));
-        System.out.println("-----");
-        System.out.println(regexHelper.isInteger("123"));
-        System.out.println(regexHelper.isInteger(" 123"));
-        System.out.println(regexHelper.isInteger("123 "));
-        System.out.println(regexHelper.isInteger("12a3"));
-        System.out.println("-----");
-        System.out.println(regexHelper.isCharacter("a"));
-        System.out.println(regexHelper.isCharacter("'a"));
-        System.out.println(regexHelper.isCharacter("'a'"));
-        System.out.println(regexHelper.isCharacter("a'"));
-        System.out.println(regexHelper.isCharacter("'ab'"));
-        System.out.println("-----");
-
     }
 }
